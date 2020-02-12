b1e3a6dd52d9092b90b8a78036d3eccf18c24bf5
AndreaG <andrea.gherardi@ddxgroup.it>
2020-02-12 13:34:33 +0100

    Second commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+    IonFab, IonFabButton, IonIcon, IonGrid, IonRow, 
+    IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import $ from 'jquery'; 
+
+function shoot() {
+    
+
+    $.ajax({
+        url: "http://gherardiandrea.altervista.org/gherardiandrea_0001.php",
+    success: function(data) {
+        alert(data);
+    }});
+}
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab Two</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonList>
-          <IonItem routerLink="/tab2/details">
-            <IonLabel>
-              <h2>Go to detail</h2>
-            </IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+    const { photos, takePhoto } = usePhotoGallery();
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                <IonTitle>Photo Gallery</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            
+            <IonContent>
+            <button onClick={shoot} >TEST</button>
+                <IonGrid>
+                    <IonRow>
+                        {photos.map((photo, index) => (
+                            <IonCol size="6" key={index}>
+                                <IonImg src={photo.base64 ?? photo.webviewPath} />
+                            </IonCol>
+                        ))}
+                    </IonRow>
+                </IonGrid>
+                <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                    <IonFabButton onClick={() => takePhoto()}>
+                        <IonIcon icon={camera}></IonIcon>
+                    </IonFabButton>
+                </IonFab>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
